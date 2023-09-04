2f57f5bf56769aadbbc3deb248ba0fbf56aef8c8
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-04 09:14:10 -0400

    Set up authorize method for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies 
-    
+    before_action :authorize
+
+    def authorize
+        
+        @curent_user = User.find_by(id: session[:user_id])
+        render json: { errors: "Not authorized"}, status: :unauthorized unless @curent_user 
+      end
+
+      
 end
