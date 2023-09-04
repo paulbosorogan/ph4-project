38db04677fccf57ea1f7b2a0637e0dcbb6ce7f27
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-04 09:07:55 -0400

    Add user context

---
+++ client/src/contexts/UserContext.js
@@ -0,0 +1,20 @@
+import {useState, useEffect, createContext} from 'react'
+
+const UserContext = createContext();
+
+function UserProvider({children}) {
+
+    const [user, setUser] = useState(null)
+console.log(user)
+    useEffect(()=> {
+        fetch('/me').then((response) => {
+            if (response.ok) {
+                response.json().then((user) => setUser(user))
+            }
+        }) 
+    }, [])
+
+    return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>
+}
+
+export {UserContext, UserProvider}
