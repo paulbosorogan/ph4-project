ee41deb2fb0a26e94de601a1b50e1ce24cab7ea1
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-04 09:15:21 -0400

    Set up methods for CRUD actions for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
     wrap_parameters format: []
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
     
     def index 
         if session[:user_id]
@@ -11,13 +13,42 @@ class ReviewsController < ApplicationController
     end
     
     def create 
-        review = Review.create!(review_params)
+        review = @curent_user.reviews.create!(review_params)
         render json: review, status: :created 
     end
 
+    def update 
+        review = @curent_user.reviews.find_by(id: params[:id])
+        review.update(review_params_update)
+        render json: review, status: :created 
+    end 
+
+
+    def destroy 
+        review = find_review
+        review.destroy
+        head :no_content
+    end
+
     private 
+    def find_review
+        review = Review.find(params[:id])
+    end
+
+    def review_params_update 
+        params.permit(:comment, :movie_id)
+    end 
 
     def review_params 
-        params.permit(:comment)
+        params.permit(:comment, :movie_id)
     end
+     
+    def render_not_found(exception)
+        render json: { error: "#{exception.model} not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end 
+    
 end
