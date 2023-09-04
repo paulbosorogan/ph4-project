965f96071bf4f93e8074bb3300e6815cdb79662f
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-04 09:18:25 -0400

    Set up attributes showing for movie

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :runtime, :director, :starring, :short, :image_url, :trailer, :genre
+  attributes :id, :title, :year, :runtime, :director, :starring, :short, :image_url, :genre
 
   has_many :reviews
 end
