09887b142a2cf97d94efdd728e0c236d56fab26f
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-05 21:06:51 -0400

    Added UserMovieSerializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :username
+  attributes :id, :comment, :username, :movie_id, :user_id
 
   has_one :movie 
   has_one :user
