e9de398353aae234a6b69f0523ec412f46c131c1
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-05 21:05:40 -0400

    Created new component - User Profile

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import MoviePoster from './MoviePoster'
+
+
+function UserProfile({user}) {
+
+    const num = user.movies.length
+    
+  return (
+
+    <div className='user-profile'>
+        <div className='user-box'>
+        <h1 className='user'>{user.username}!</h1>
+        </div>
+        <hr></hr>
+        <h2>We hope you enjoy our rating application!</h2>
+        <p>Below you will find a list with all the movies you have interacted so far. You have rated <b className='num'>{num}</b> {num>1? 'movies' : 'movie'}! Way to go!</p>
+        {user.movies.map(movie=> (
+            <div className='user-movies' key={movie.id}>
+                <div className='user-movies-info'>
+                    <h2>{movie.title}</h2>
+                    <p>Director/s: {movie.director}</p>
+                    <p><small>Year: {movie.year}</small></p>
+                    <MoviePoster movie={movie}/>
+                </div>
+            </div>))}
+    </div>
+  )
+}
+
+export default UserProfile
