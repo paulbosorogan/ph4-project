09ff34a34415599701be2ec7f089af1bd9d5a881
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-05 21:04:27 -0400

    Updated active record associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
 
     has_many :reviews
+    has_many :movies, through: :reviews
+
+    # //create user profile page - adds movies that the user has reviewed
 end
