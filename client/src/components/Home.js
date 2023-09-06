e8b2cb6c631319c824c31dfbbd81789cee8a277e
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-05 21:10:47 -0400

    Updated greeting message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,13 +11,13 @@ import Header from './Header'
     <div className='homepage'>
     <Header/>
       <hr></hr>
-      <h1 className='greeting'>Welcome, {user.username}!</h1>
-  
+      <h1 className='greeting'>Greetings, {user.username}!</h1>
       <hr></hr>
       <p >We are pleased to have you with us on our new movie rating website.</p>
       <p className='greeting-text'>If you are a movie lover, you came to the right place! </p>
       <p className='greeting-text'>Please feel free to enjoy our premium selection of movies.</p>
       <br></br>
+  
     </div>
     </>
   )
