4775945f7244563a289e5e9a499b972f55552e3b
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-05 21:03:54 -0400

    Updated validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
-    validates :comment, presence: true, length: { maximum: 100}
     
+    validates :comment, presence: true, length: { maximum: 100}
+    validates :user_id, uniqueness: { scope: :movie_id, message: "Limited 1 review per movie" }
+
     belongs_to :user 
     belongs_to :movie
 
