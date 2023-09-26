0c860a5ab2a925924f6d6bc51034bc2fad88fdb3
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-09-26 19:05:09 -0400

    Updated UserProfile movie cards

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -14,15 +14,15 @@ function UserProfile({user}) {
         </div>
         <hr></hr>
         <h2>We hope you enjoy our rating application!</h2>
-        <p>Below you will find a list with all the movies you have interacted so far. You have reviewed <b className='num'>{num}</b> {num==1 ? 'movie' : 'movies'}! Way to go!</p>
+        <p>Below you will find a list with all the movies you have interacted so far. You have reviewed <b className='num'>{num}</b> {num===1 ? 'movie' : 'movies'}! Way to go!</p>
         {user.movies.map(movie=> (
             <div className='user-movies' key={movie.id}>
                 <div className='user-movies-info'>
                     <h2>{movie.title}</h2>
                     <p>Director/s: {movie.director}</p>
                     <p><small>Year: {movie.year}</small></p>
-                    <MoviePoster movie={movie}/>
                 </div>
+                <MoviePoster movie={movie}/>
             </div>))}
     </div>
   )
