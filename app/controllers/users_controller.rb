f220fe3c143a250f048859b59bc8edd8d5ee8b7a
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-10-16 14:16:52 -0400

    Update controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
-before_action :authorize 
 skip_before_action :authorize, only: [:create]
 wrap_parameters format: []
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity 
 
-def index 
-    users = User.all 
-    render json: users
-end
 
 def create 
     user = User.create(user_params)
@@ -29,9 +23,5 @@ private
 def user_params 
     params.permit(:username, :password, :password_confirmation)
 end
-
-def render_unprocessable_entity(invalid)
-    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
-end 
 
 end
