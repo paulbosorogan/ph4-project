5f7d764cf30a8af42d651dbde2420b155ebeb247
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-10-16 14:15:41 -0400

    Updated the unprocessable entity method and no record found method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies 
     before_action :authorize
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity 
 
     def authorize
-        
         @curent_user = User.find_by(id: session[:user_id])
         render json: { errors: "Not authorized"}, status: :unauthorized unless @curent_user 
-      end
+    end
 
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end 
       
 end
