a7d2a02c0e07f931ce5c7a161ded1b78c4564226
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-10-16 14:16:11 -0400

    Update controller actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,20 @@
 class MoviesController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found 
+    skip_before_action :authorize, only: [:index]
 
-    
     def index
         movies = Movie.all 
         render json: movies, status: :ok
     end
 
-    def show 
-        movie = Movie.find(params[:id])
-        render json: movie, status: :ok
+    def create 
+        movie = Movie.create!(movie_params)
+        render json: movie, status: :created
     end
 
     private 
-
-    def render_not_found 
-        render json: { errors: "Movie not found"}, status: :not_found
+    def movie_params 
+        params.require(:movie).permit(:title, :year, :runtime, :director, :starring, :short, :trailer, :image_url, :genre)
     end
+
+    
 end
